971f3c134eda1549fd75fe447792722437c8e872
Matheus Sodré <48135751+Sodrezera@users.noreply.github.com>
2019-06-14 12:49:19 -0300

    Update exercicio1.php

--- exercicio1.php
+++ exercicio1.php
@@ -1,5 +1,4 @@
-    
-<? php
+<?php
     função  graus ( $ temp ) {
         $ Celsius  = ( $ temperatura  -  32 ) * ( 5 / 9 );
         $ msg  =  " a temperatura $ temp "  .  " º "  .  " está em fahrenheit, convertendo para celsius, é igual a: $ celsius "  .  " º celsius \ n " ;
